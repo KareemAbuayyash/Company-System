3a3293c7f23d0decbb86c6ee53436fa94a6bee74
Ahmad Alarjah <160240167+Ahmad-Al3arja@users.noreply.github.com>
2025-07-30 20:58:15 +0300

    Delete CompanySystem.Business/Interfaces directory

--- CompanySystem.Business/Interfaces/IRoleRepository.cs
+++
@@ -1,9 +0,0 @@
-using CompanySystem.Data.Models;
-
-namespace CompanySystem.Business.Interfaces
-{
-    public interface IRoleRepository : IRepository<Role>
-    {
-        Task<Role?> GetByNameAsync(string roleName);
-    }
-} 
